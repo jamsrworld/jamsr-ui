b84c4a01cf8dd94b017b820f96cd679242754320
adityaa72 <adityah1357908642@gmail.com>
2024-05-21 20:23:17 +0530

    feat: added test

---
+++ packages/components/src/button/__tests__/button.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, test } from "vitest";
+import { Button } from "..";
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+describe("Button", () => {
+  it("test", () => {
+    expect(sum(1, 2)).toBe(4);
+  });
+  it("test2", () => {
+    expect(sum(1, 2)).not.toBe(4);
+  });
+  test("test3", () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+  it("check button render correctly", () => {
+    render(<Button>hiii</Button>);
+    expect(screen.getByRole("button")).toBeDefined()
+  });
+});
--- packages/components/src/button/stories/button.stories.tsx
+++ packages/components/src/button/stories/button.stories.tsx
@@ -1,6 +1,6 @@
 import { type Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import { Button } from "../Button";
+import { Button } from "../src/Button";
 
 const meta = {
   title: "Components/Button",

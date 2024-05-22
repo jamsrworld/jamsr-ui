b15bfa0daa4d2dc6bb39abe8757703223485e79a
adityaa72 <adityah1357908642@gmail.com>
2024-05-22 14:13:18 +0530

    chore: setup done

--- packages/components/src/button/src/Button.tsx
+++ packages/components/src/button/src/Button.tsx
@@ -5,16 +5,11 @@ import { type ComponentPropsWithoutRef } from "react";
 
 type ButtonProps = ComponentPropsWithoutRef<"button">;
 
-let a = 3;
-const sum = (a: number) => a;
-
-console.log(sum("a"))
-
 export const Button = ({ children, className = "", ...rest }: ButtonProps) => (
   <button
     type="button"
     className={cn(
-      `rounded-lg border border-gray-500 bg-black px-4 py-2 text-pink-50 ring-2 ring-pink-400 md:bg-white`,
+      `rounded-lg border border-gray-500 bg-black px-4 py-2 text-pink-50 ring-2 ring-pink-400`,
       className,
     )}
     {...rest}

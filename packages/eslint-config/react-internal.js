996f9aa4e764044d2995fbe1454594b4810ad917
adityaa72 <adityah1357908642@gmail.com>
2024-05-22 12:14:57 +0530

    chore: setup eslint

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -10,8 +10,21 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
-  plugins: ["only-warn"],
+  plugins: ["@typescript-eslint", "only-warn", "import"],
+  extends: [
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:tailwindcss/recommended",
+    "eslint:recommended",
+    "airbnb",
+    "airbnb-base",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "prettier",
+    "async",
+    "async/typescript",
+    "eslint-config-turbo",
+  ],
   globals: {
     React: true,
     JSX: true,
@@ -20,10 +33,25 @@ module.exports = {
     browser: true,
   },
   settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
     "import/resolver": {
       typescript: {
         project,
       },
+    },
+    tailwindcss: {
+      // These are the default values but feel free to customize
+      callees: ["classnames", "clsx", "ctl", "cn"],
+      config: "tailwind.config.js", // returned from `loadConfig()` utility if not provided
+      cssFiles: ["**/*.css", "!**/node_modules", "!**/.*", "!**/dist", "!**/build"],
+      cssFilesRefreshRate: 5_000,
+      removeDuplicates: true,
+      skipClassAttribute: false,
+      whitelist: [],
+      tags: [], // can be set to e.g. ['tw'] for use in tw`bg-blue`
+      classRegex: "^class(Name)?$", // can be modified to support custom attributes. E.g. "^tw$" for `twin.macro`
     },
   },
   ignorePatterns: [
@@ -34,6 +62,19 @@ module.exports = {
   ],
   overrides: [
     // Force ESLint to detect .tsx files
-    { files: ["*.js?(x)", "*.ts?(x)"] },
+    { files: ["*.js?(x)", "*.ts?(x)"], parser: "@typescript-eslint/parser" },
   ],
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        fixStyle: "inline-type-imports",
+        prefer: "type-imports",
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "react/function-component-definition": "off",
+    "react/jsx-props-no-spreading": "off",
+  },
 };

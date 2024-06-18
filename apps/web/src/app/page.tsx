328c228f218c1c5d95dd16b7d38e72dd6a5005b7
adityaa72 <adityah1357908642@gmail.com>
2024-06-18 18:33:33 +0530

    other: other

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,4 @@
-"use client";
-
 import { Button } from "@jamsr-ui/react";
-
-const defaultContent =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
 const page = () => {
   return (
